2251949615a1decf04c51b906119304a08f3206c
MrSalim86 <99346722+MrSalim86@users.noreply.github.com>
2022-03-28 11:30:15 +0200

    Added DieselTruck.java

---
+++ src/com/company/DieselTruck.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class DieselTruck extends DieselCar {
+
+    public DieselTruck(String RegistrationNumber, String Make, String Model, int NumberOfDoors, int kmPrLitre, boolean hasParticleFilter) {
+        super(RegistrationNumber, Make, Model, NumberOfDoors, kmPrLitre, hasParticleFilter);
+    }
+}
