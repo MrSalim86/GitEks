99841b117e0b9c5f91441943d1bcf112b215190b
MrSalim86 <99346722+MrSalim86@users.noreply.github.com>
2022-03-29 14:58:01 +0200

    Added DieselTruck.java

--- src/com/company/MainFeature.java
+++ src/com/company/MainFeature.java
@@ -1,3 +1,5 @@
+package com.company;
+
 public class MainFeature {
     @Override
     public String toString() {
--- src/com/company/NewFeature.java
+++ src/com/company/NewFeature.java
@@ -1,3 +1,5 @@
+package com.company;
+
 public class NewFeature {
     @Override
     public String toString() {
